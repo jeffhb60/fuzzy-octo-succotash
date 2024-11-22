78332163c4429c6f40ad4f2f808c6447a98e5d8c
Jeff Butt <jbutt2@wgu.edu>
2024-11-21 22:12:14 -0500

    Add deleteCategory functionality to CategoryController and CategoryService Added a DELETE endpoint in CategoryController to delete a category by its categoryId. Updated CategoryService interface to include a deleteCategory method for category deletion logic. Implemented deleteCategory in CategoryServiceImpl to remove a category from the list based on its ID. Enhanced error handling to return a message if the category with the specified ID is not found. Improved overall functionality by introducing support for category deletion.

--- src/main/java/com/jeffhb60/ecomshoppingcart/controller/CategoryController.java
+++ src/main/java/com/jeffhb60/ecomshoppingcart/controller/CategoryController.java
@@ -3,10 +3,7 @@ package com.jeffhb60.ecomshoppingcart.controller;
 import com.jeffhb60.ecomshoppingcart.model.Category;
 import com.jeffhb60.ecomshoppingcart.service.CategoryService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +23,12 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "Category added successfully";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        String status = categoryService.deleteCategory(categoryId);
+        return status;
     }
 
 }
