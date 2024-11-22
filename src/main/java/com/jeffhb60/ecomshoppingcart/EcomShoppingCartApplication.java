b0a577a5f69a3f3d39872caa9db20936446921c6
Jeff Butt <jbutt2@wgu.edu>
2024-11-21 21:29:00 -0500

    Initial Commit

---
+++ src/main/java/com/jeffhb60/ecomshoppingcart/EcomShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.jeffhb60.ecomshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomShoppingCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomShoppingCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeffhb60/ecomshoppingcart/EcomShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffhb60.ecomshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
